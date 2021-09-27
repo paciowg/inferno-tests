0f1207f8d3480adce0ecb373557f129cb888b357
Brian Meshell <bmeshell@mitre.org>
2021-09-27 11:26:10 -0400

    abs path to log file

--- lib/pacio-adi/adi_document_reference.rb
+++ lib/pacio-adi/adi_document_reference.rb
@@ -4,7 +4,7 @@ module PacioAdi
       title 'ADI Document Reference (PACIO Advance Directives) Tests'
 
       require "logger"
-      logger = Logger.new('log.txt')
+      logger = Logger.new('/home/bmeshell/inferno-tests/log.txt')
       logger.debug("this is a debug log")
       logger.error("this is an error log")
         
