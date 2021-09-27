2713b9c691f25142609958a934b909ffac131b9c
Brian Meshell <bmeshell@mitre.org>
2021-09-27 11:15:27 -0400

    use logger to write to text file

--- lib/pacio-adi/adi_document_reference.rb
+++ lib/pacio-adi/adi_document_reference.rb
@@ -4,8 +4,8 @@ module PacioAdi
       title 'ADI Document Reference (PACIO Advance Directives) Tests'
 
       require "logger"
-      logger = Logger.new(STDOUT)
-      logger.level = Logger::ERROR
+      logger = Logger.new('log.txt')
+      logger.debut("this is a debut log")
       logger.error("this is an error log")
         
       description 'Verify support for the server capabilities required by the ADI Document Reference profile.'
