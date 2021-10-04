7460a608f96a207126c8f31009cc10ff477453ea
Brian Meshell <bmeshell@mitre.org>
2021-10-04 11:38:21 -0400

    test update label

--- lib/pacio-adi/adi_document_reference.rb
+++ lib/pacio-adi/adi_document_reference.rb
@@ -1,7 +1,7 @@
 module PacioAdi
     class AdiDocumentReference < Inferno::TestGroup
 
-      title 'ADI Document Reference (PACIO Advance Directives) Tests'
+      title 'ADI Document Reference (PACIO Advance Directives) Tests v1'
 
       require "logger"
       logger = Logger.new(STDOUT)
