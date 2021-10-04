83ae3cc9259233f6d82b9932b596665c7c346691
Brian Meshell <bmeshell@mitre.org>
2021-10-04 11:59:36 -0400

    copy doc ref test to top level kit

--- lib/pacio_adi_test_kit.rb
+++ lib/pacio_adi_test_kit.rb
@@ -20,6 +20,35 @@ module PacioAdi
       title 'Capability Statement'
       description 'Verify that the server has a CapabilityStatement'
 
+
+      test do
+        title 'Server returns correct ADI Document Reference resource from the ADI Document Reference read interaction'
+        description %(
+          This test will verify that ADI Document Reference resources can be read from the server.
+        )
+        # link http://hl7.org/fhir/us/pacio-adi/StructureDefinition/PADI-DocumentReference
+  
+      makes_request :adi_document_reference
+
+
+        #todo: move assert valid 200 response code to separate tests
+        
+  
+        run do
+          logger.warn("") #whitespace for readable logs
+          logger.warn("begin DocumentReference read test")
+          fhir_read(:DocumentReference, adi_document_reference_id, name: :adi_document_reference)
+          
+          assert_response_status(200)
+          assert_resource_type(:DocumentReference)
+          assert resource.id == adi_document_reference_id,
+                 "Requested resource with id #{adi_document_reference_id}, received resource with id #{resource.id}"
+          logger.warn("end of DocumentReference read test")
+        end
+      end
+
+
+
       test do
         id :capability_statement_read
         title 'Read CapabilityStatement'
