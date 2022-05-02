fa8325399d476e698935cdec2a9d03798f740d7b
Brian Meshell <bmeshell@mitre.org>
2022-05-02 13:19:02 -0400

    fp_collection is a profile of Observation resource. Fix name where applicable.

--- lib/pacio-fp/fp_collection.rb
+++ lib/pacio-fp/fp_collection.rb
@@ -13,13 +13,13 @@ module PacioFp
           This test will verify that Functional Performance Collection resources can be read from the server.
         )
   
-      makes_request :collection
+      makes_request :observation
   
       run do
-        fhir_read(:collection, fp_collection_id, name: :collection)
+        fhir_read(:observation, fp_collection_id, name: :collection)
   
         assert_response_status(200)
-          assert_resource_type(:collection)
+          assert_resource_type(:observation)
           assert resource.id == fp_collection_id,
                  "Requested resource with id #{fp_collection_id}, received resource with id #{resource.id}"
         end
@@ -30,7 +30,7 @@ module PacioFp
         description %(
           This test will validate that the Functional Performance Collection resource returned from the server matches the PACIO Functional Performance Collection profile.
         )
-        uses_request :collection
+        uses_request :observation
         
         run do
           assert_valid_resource(profile_url: 'http://hl7.org/fhir/us/pacio-fp/StructureDefinition/fp-collection')
@@ -44,10 +44,10 @@ module PacioFp
           This test will validate that the FHIR server supports searching PACIO Functional Performance Collection profile by ID.
         )
   
-        uses_request :collection
+        uses_request :observation
   
         run do
-          fhir_search :collection, params: { _id: resource.id }
+          fhir_search :observation, params: { _id: resource.id }
   
           assert_response_status(200)
           assert_resource_type('Bundle')
