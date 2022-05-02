8743bbad46645e0c19e76678980080beea53e483
Brian Meshell <bmeshell@mitre.org>
2022-05-02 18:50:33 -0400

    fix fp tests to profile the correct base FHIR resources

--- lib/pacio-fp/fp_narrative_history_of_status.rb
+++ lib/pacio-fp/fp_narrative_history_of_status.rb
@@ -13,13 +13,13 @@ module PacioFp
           This test will verify that Functional Performance Narrative History of Status resources can be read from the server.
         )
   
-      makes_request :narrative_history_of_status
+      makes_request :observation
   
       run do
-        fhir_read(:narrative_history_of_status, fp_narrative_history_of_status_id, name: :narrative_history_of_status)
+        fhir_read(:observation, fp_narrative_history_of_status_id, name: :observation)
   
         assert_response_status(200)
-          assert_resource_type(:narrative_history_of_status)
+          assert_resource_type(:observation)
           assert resource.id == fp_narrative_history_of_status_id,
                  "Requested resource with id #{fp_narrative_history_of_status_id}, received resource with id #{resource.id}"
         end
@@ -30,7 +30,7 @@ module PacioFp
         description %(
           This test will validate that the Functional Performance Narrative History of Status resource returned from the server matches the PACIO Functional Performance Narrative History of Status profile.
         )
-        uses_request :narrative_history_of_status
+        uses_request :observation
         
         run do
           assert_valid_resource(profile_url: 'http://hl7.org/fhir/us/pacio-fp/StructureDefinition/fp-narrative-history-of-status')
@@ -44,10 +44,10 @@ module PacioFp
           This test will validate that the FHIR server supports searching PACIO Functional Performance Narrative History of Status profile by ID.
         )
   
-        uses_request :narrative_history_of_status
+        uses_request :observation
   
         run do
-          fhir_search :narrative_history_of_status, params: { _id: resource.id }
+          fhir_search :observation, params: { _id: resource.id }
   
           assert_response_status(200)
           assert_resource_type('Bundle')
--- lib/pacio-fp/fp_use_of_device.rb
+++ lib/pacio-fp/fp_use_of_device.rb
@@ -13,13 +13,13 @@ module PacioFp
           This test will verify that Functional Performance Use of Device resources can be read from the server.
         )
   
-      makes_request :use_of_device
+      makes_request :DeviceUseStatement
   
       run do
-        fhir_read(:use_of_device, fp_use_of_device_id, name: :use_of_device)
+        fhir_read(:DeviceUseStatement, fp_use_of_device_id, name: :DeviceUseStatement)
   
         assert_response_status(200)
-          assert_resource_type(:use_of_device)
+          assert_resource_type(:DeviceUseStatement)
           assert resource.id == fp_use_of_device_id,
                  "Requested resource with id #{fp_use_of_device_id}, received resource with id #{resource.id}"
         end
@@ -30,7 +30,7 @@ module PacioFp
         description %(
           This test will validate that the Functional Performance Use of Device resource returned from the server matches the PACIO Functional Performance Use of Device profile.
         )
-        uses_request :use_of_device
+        uses_request :DeviceUseStatement
         
         run do
           assert_valid_resource(profile_url: 'http://hl7.org/fhir/us/pacio-fp/StructureDefinition/fp-use-of-device')
@@ -44,10 +44,10 @@ module PacioFp
           This test will validate that the FHIR server supports searching PACIO Functional Performance Use of Device profile by ID.
         )
   
-        uses_request :use_of_device
+        uses_request :DeviceUseStatement
   
         run do
-          fhir_search :use_of_device, params: { _id: resource.id }
+          fhir_search :DeviceUseStatement, params: { _id: resource.id }
   
           assert_response_status(200)
           assert_resource_type('Bundle')
